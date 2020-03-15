c844be74bb4e5404bb431dc5e90ca1e0f4190f3b
libmurg <9257773+libmurg@users.noreply.github.com>
2020-03-15 18:26:09 +0000

    black-formatted

--- lib/mail.py
+++ lib/mail.py
@@ -3,12 +3,14 @@ import smtplib
 import imaplib
 import email
 
+
 class MailBot:
     """
     Sending and receiving mail
     
     This contains all methods to interact with smtp and imap servers
     """
+
     # Configs - should not need to change
     ssl_port = 465
     smtp_server = "smtp.gmail.com"
@@ -18,10 +20,10 @@ class MailBot:
     def read_email(cls, pp, email_id):
         with imaplib.IMAP4_SSL(cls.imap_server) as im:
             im.login(pp.bot_email, pp.bot_pass)
-            im.select('inbox')
-            
+            im.select("inbox")
+
             email_id_bytes = str.encode(str(email_id))
-            _, response = im.fetch(email_id_bytes, '(RFC822)')
+            _, response = im.fetch(email_id_bytes, "(RFC822)")
             try:
                 return email.message_from_bytes(response[0][1])
             except TypeError:
@@ -30,18 +32,18 @@ class MailBot:
     @classmethod
     def send_email(cls, pp, msg):
         context = ssl.create_default_context()
-        msg['From'] = pp.bot_email
+        msg["From"] = pp.bot_email
 
         with smtplib.SMTP_SSL(cls.smtp_server, cls.ssl_port, context=context) as server:
             server.login(pp.bot_email, pp.bot_pass)
             server.send_message(msg)
-           
+
     @classmethod
     def get_email_ids(cls, pp):
         with imaplib.IMAP4_SSL(cls.imap_server) as im:
             im.login(pp.bot_email, pp.bot_pass)
-            im.select('inbox')
-            _, email_ids = im.search(None, 'ALL')
+            im.select("inbox")
+            _, email_ids = im.search(None, "ALL")
             return [int(i) for i in email_ids[0].split()]
 
 
@@ -49,41 +51,42 @@ class PrepBot:
     """
     Prepares/decodes an email.message.EmailMessage instance
     """
+
     @staticmethod
     def decipher_payload(msg):
         msg_pl = msg.get_payload()
         if isinstance(msg_pl, str):
             return msg_pl
-        elif type(msg_pl)==list:
+        elif type(msg_pl) == list:
             return msg_pl[0].get_payload()
 
     @staticmethod
     def decipher_sender(msg):
-        msg_sender = msg['From']
-        msg_sender_email = msg_sender.split('<')[-1].replace('>','')
-        msg_sender_name = msg_sender.split('<')[0].strip()
+        msg_sender = msg["From"]
+        msg_sender_email = msg_sender.split("<")[-1].replace(">", "")
+        msg_sender_name = msg_sender.split("<")[0].strip()
         return (msg_sender_name, msg_sender_email)
 
     @staticmethod
     def construct_msg(recipient_email, text, subject=None):
         # Prepare message
         msg = email.message.EmailMessage()
-        msg['Subject'] = subject
-        msg['To'] = recipient_email
+        msg["Subject"] = subject
+        msg["To"] = recipient_email
         msg.set_content(text)
         return msg
-            
+
     @staticmethod
-    def construct_fwd_email(msg, recipient_email, pre_text=''):
+    def construct_fwd_email(msg, recipient_email, pre_text=""):
         msg_text = PrepBot.decipher_payload(msg)
 
-        new_msg_text = f"{pre_text} \r\n\r\n ----------------------- \r\n\r\n {msg_text}"
+        new_msg_text = (
+            f"{pre_text} \r\n\r\n ----------------------- \r\n\r\n {msg_text}"
+        )
         new_msg_subject = f"AutoFW: {msg['Subject']}"
 
         fw_msg = PrepBot.construct_msg(
-            recipient_email=recipient_email,
-            text=new_msg_text,
-            subject=new_msg_subject
+            recipient_email=recipient_email, text=new_msg_text, subject=new_msg_subject
         )
         return fw_msg
 
@@ -92,15 +95,16 @@ class ScanBot:
     """
     Searches for hallmarks
     """
+
     @staticmethod
     def scan_text(msg, srchtxt):
         msgtxt = PrepBot.decipher_payload(msg)
         return srchtxt.lower() in msgtxt.lower()
 
     @staticmethod
-    def scan_sender(msg, srchtxt, how='email'):
+    def scan_sender(msg, srchtxt, how="email"):
         (sender_name, sender_email) = PrepBot.decipher_sender(msg)
-        if how=='email':
+        if how == "email":
             return srchtxt.lower() in sender_email.lower()
-        elif how=='name':
+        elif how == "name":
             return srchtxt.lower() in sender_name.lower()
